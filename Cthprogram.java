a1033b56ee49fad3b54f8bf4f83b53ebef77732a
Alexander9076 <alexanderryuu60@gmail.com>
2020-01-12 03:00:17 +0700

    Initial Commit

---
+++ Cthprogram.java
@@ -0,0 +1,23 @@
+package cthprogram;
+public class Cthprogram {
+     //Aritmatika perhitungan
+    public static void main(String[] args) {
+        // deklarasi variabel
+        int a,b,c;
+        double kali,bagi,tambah,kurang;
+        // memberikan nilai pada variabel      
+        a=10;
+        b=6;  
+        c=23;
+        //menentukan rumus
+        kali=a*b;
+        bagi=c/b;
+        tambah=c+c;
+        kurang=c-a;
+        //Cetak nilai 
+        System.out.println("kali: "+kali);
+        System.out.println("bagi: "+bagi);
+        System.out.println("tambah: "+tambah);
+        System.out.println("kurang: "+kurang);
+    }   
+}
