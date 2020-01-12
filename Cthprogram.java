094505174d1063ccfd831a19481b66117564ed28
Alexander9076 <alexanderryuu60@gmail.com>
2020-01-12 23:59:54 +0700

    penambahan fungsi kali

--- Cthprogram.java
+++ Cthprogram.java
@@ -11,13 +11,9 @@ public class Cthprogram {
         c=23;
         //menentukan rumus
         kali=a*b;
-        bagi=c/b;
-        tambah=c+c;
-        kurang=c-a;
+
         //Cetak nilai 
         System.out.println("kali: "+kali);
-        System.out.println("bagi: "+bagi);
-        System.out.println("tambah: "+tambah);
-        System.out.println("kurang: "+kurang);
+
     }   
 }
